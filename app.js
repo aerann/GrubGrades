b6beffbda02641bbd28db47494811f34ffc124ab
Ann Shi <annshi204a@gmail.com>
2023-01-07 15:57:18 -0500

    implemented bootstrap to project

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
 const Dish = require('./models/dish');
 const { rmSync } = require('fs');
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/grub-grades') //shopApp database
 
 const app = express()
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join (__dirname, 'views'))
 
