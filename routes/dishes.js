6e22e10f3c9d3c47f7c59768d71caf4480038daf
Ann Shi <annshi204a@gmail.com>
2023-02-13 19:21:04 -0500

    multer middleware to upload files

--- routes/dishes.js
+++ routes/dishes.js
@@ -5,9 +5,19 @@ const catchAsync = require('../utils/catchAsync')
 const {isLoggedIn, isAuthor, validateDish} = require('../middleware')
 const Dish = require('../models/dish')
 
+//used for parsing images
+const multer = require('multer') 
+const upload = multer({ dest: 'uploads/'}) //sets destination for your images
+
+
+
 router.route('/')
     .get(catchAsync(dishes.index))
-    .post(isLoggedIn, validateDish, catchAsync(dishes.createDish))
+    // .post(isLoggedIn, validateDish, catchAsync(dishes.createDish))
+    .post(upload.array('image'), (req,res) => {
+        console.log(req.body, req.files)
+        res.send('it worked')
+    })
 
 //needs to go before id route, or else will mistaken new as an id
 router.get('/new', isLoggedIn, dishes.renderNewForm)
